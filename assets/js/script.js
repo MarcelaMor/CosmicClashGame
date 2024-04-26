3f51b969ebeea7f6c764d3ea69f90ae360d0a82a
Marcela Morales <marcelamorales90@gmail.com>
2024-04-26 21:29:34 +0000

    added a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,17 @@ const choiceBtns = document.querySelectorAll(".choiceBtn");
 let competitor;
 let computer;
 let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    competitor = button.textContent;
+    computerTurn(); 
+
+
+}));
+
+function  computerTurn(){
+
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+}
