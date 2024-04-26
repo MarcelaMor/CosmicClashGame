72b4d59be25e1831d6fc9d958af90e98bf58cc08
Marcela Morales <marcelamorales90@gmail.com>
2024-04-26 20:52:05 +0000

    added som divs and class

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const playerText = document.querySelector("#competitorText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+let competitor;
+let computer;
+let result;
