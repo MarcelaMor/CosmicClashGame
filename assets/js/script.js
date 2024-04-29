4bfbd2ba127ba42f83f381dacd410a49488b7d01
Marcela Morales <marcelamorales90@gmail.com>
2024-04-29 19:11:32 +0000

    added more html elements and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
+//Selecting Dom elements
 const userText = document.querySelector("#userText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
+const userCounter = document.querySelectorAll("#userPoints");
+const computerCounter = document.querySelectorAll("#computerPoints");
 
 let user;
 let computer;
 let result;
+let computerPoints;
+let userPoints;
+let draws;
+
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
 
@@ -14,6 +21,7 @@ choiceBtns.forEach(button => button.addEventListener("click", () => {
     userText.textContent = `User: ${user}`;
     computerText.textContent = `Computer: ${computer}`;
     resultText.textContent = checkWinner();
+    result = checkWinner();
 }));
 
 
@@ -57,36 +65,53 @@ function checkWinner() {
         return "It's a tie!";
     }
     else if (computer == "Rock") {
-        return (user == "Paper") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Paper") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Rock") {
-        return (user == "Spock") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Spock") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Paper") {
-        return (user == "Scissor") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Scissor") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Paper") {
-        return (user == "Lizard") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Lizard") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Scissors") {
-        return (user == "Rock") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Rock") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Scissors") {
-        return (user == "Spock") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Spock") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Lizard") {
-        return (user == "Rock") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Rock") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Lizard") {
-        return (user == "Scissors") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Scissors") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Spock") {
-        return (user == "Lizard") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Lizard") ? "Congratulations!" : "Aww you lost!"
     }
     else if (computer == "Spock") {
-        return (user == "Paper") ? "Congratulations!" : "Aww You lost!"
+        return (user == "Paper") ? "Congratulations!" : "Aww you lost!"
     }
 
 
+
+
+/* Update score function */
+function setPoints(result) {  // Update score based on result
+    if (result === "Congratulations!") {    // Check result and update score
+        userScore++;    // Incriment users score if they won
+    } else if (result === "Aww you lost!") {
+        opponentScore++;    // Incriment opponent score if they won
+    } else if (result === "It's a tie!") {
+        drawScore++;    // Incriment draw score if they game ended in draw
+    }
+
+
+    userScoreText.textContent = `User: ${userScore}`;   // Update user score text
+    opponentScoreText.textContent = `Opponent: ${opponentScore}`;   // Update opponent score text
+    drawScoreText.textContent = `Draw: ${drawScore}`;   // Update draw score text
+}
 
 }
