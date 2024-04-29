dc474b38380469be352b1ab4e48fc601b8e1b660
Marcela Morales <marcelamorales90@gmail.com>
2024-04-29 13:53:43 +0000

    added more switch variations

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,51 +10,59 @@ let result;
 choiceBtns.forEach(button => button.addEventListener("click", () => {
 
     user = button.textContent;
-    computerTurn(); 
-    userText.textContent = `User: ${user}` ;
-    computerText.textContent = `Computer: ${computer}` ;
+    computerTurn();
+    userText.textContent = `User: ${user}`;
+    computerText.textContent = `Computer: ${computer}`;
     resultText.textContent = checkWinner();
 }));
 
 
-function  computerTurn(){
+function computerTurn() {
 
-    //Main Game Funtion
-    /** 3 choices, remeber to change this when adding lizard, spock */
+    //Math floor = rounds down and returns the largest number 
+    // Math random = random number from 0 - 0.99 
 
-    const randNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randNum);
- 
-    switch(randNum){
+    const randNum = Math.floor(Math.random() * 5) + 1;
+
+    switch (randNum) {   // Allows me to execute one of these code blocks. 
+         i give it      //It performs depending on what variations/condition it has
 
         case 1:
-        computer = "Rock"
-        break;
+            computer = "Rock"
+            break;     //The break statement terminates the switch statement, if failed it continues to the next case
 
         case 2:
-        computer = "Papper"
-        break;
+            computer = "Papper"
+            break;
 
         case 3:
-        computer = "Scissors"
-        break;
+            computer = "Scissors"
+            break;
+
+        case 4:
+            computer = "Lizard"
+            break;
+
+        case 5:
+            computer = "Spock"
+            break;
 
     }
 }
 
-     /** Winner funtions and rules */ 
-     //Dont forget to add 2 more
-function checkWinner(){
-    if(user == computer){
+/** Winner funtions and rules */
+//Dont forget to add 2 more
+function checkWinner() {
+    if (user == computer) {
         return "It's a tie!";
     }
-    else if(computer == "Rock"){
-        return (user == "Papper") ? "Congratulations!" : "Aww You lost!" 
+    else if (computer == "Rock") {
+        return (user == "Papper") ? "Congratulations!" : "Aww You lost!"
     }
-    else if(computer == "Paper"){
+    else if (computer == "Paper") {
         return (user == "Scissor") ? "Congratulations!" : "Aww You lost!"
     }
-    else if(computer == "Scissors"){
+    else if (computer == "Scissors") {
         return (user == "Rock") ? "Congratulations!" : "Aww You lost!"
     }
 }
