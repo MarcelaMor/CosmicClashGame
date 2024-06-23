cf3a17b42e0c89d2369fc2e774d5d44b99dfc271
MarcelaMor <marcelamorales90@gmail.com>
2024-06-23 18:59:04 +0200

    refactor: rewrite "checkwinner" function
    This fixes the issue with some user-winning conditions not being honored.  Also it's much easier now to see each choice with its winning conditions in the code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,129 +1,117 @@
 //Selecting Dom elements
-const userText = document.querySelector("#userText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
-const userCounter = document.querySelector("#userPoints");
-const computerCounter = document.querySelector("#computerPoints");
-const tieCounter = document.querySelector("#ties");
+const userText = document.querySelector('#userText')
+const computerText = document.querySelector('#computerText')
+const resultText = document.querySelector('#resultText')
+const choiceBtns = document.querySelectorAll('.choiceBtn')
+const userCounter = document.querySelector('#userPoints')
+const computerCounter = document.querySelector('#computerPoints')
+const tieCounter = document.querySelector('#ties')
 
 //Variable declarations
-let user;
-let computer;
-let computerPoints = 0;
-let userPoints = 0;
-let tiePoints = 0;
-let timesPlayed = 0;
-const maxRounds = 7;
+let user
+let computer
+let computerPoints = 0
+let userPoints = 0
+let tiePoints = 0
+let timesPlayed = 0
+const maxRounds = 7
 
 //Adding click listener to buttons
-choiceBtns.forEach(button => button.addEventListener("click", () => {
-
-    user = button.textContent;
-    computerTurn();
-    userText.textContent = `User: ${user}`;
-    computerText.textContent = `Computer: ${computer}`;
-    result = checkWinner();
-    resultText.textContent = result;
-    timesPlayed++;
+choiceBtns.forEach((button) =>
+  button.addEventListener('click', () => {
+    user = button.textContent
+    computerTurn()
+    userText.textContent = `User: ${user}`
+    computerText.textContent = `Computer: ${computer}`
+    result = checkWinner()
+    resultText.textContent = result
+    timesPlayed++
     if (timesPlayed === 7) {
-        finishGame();
+      finishGame()
     } else {
-        setPoints(result);
+      setPoints(result)
     }
-
-}));
-
+  })
+)
 
 function computerTurn() {
+  //Math floor = rounds down and returns the largest number
+  // Math random = random number from 0 - 0.99
 
-    //Math floor = rounds down and returns the largest number 
-    // Math random = random number from 0 - 0.99 
+  const randNum = Math.floor(Math.random() * 5) + 1
 
-    const randNum = Math.floor(Math.random() * 5) + 1;
+  switch (
+    randNum // Allows me to execute one of these code blocks.
+  ) {
+    //It performs depending on what "argument" it receives from "randNum" variable
 
-    switch (randNum) {   // Allows me to execute one of these code blocks. 
-        //It performs depending on what "argument" it receives from "randNum" variable
+    case 1:
+      computer = 'Rock'
+      break //The break statement terminates the switch statement, if failed it continues to the next case
 
-        case 1:
-            computer = "Rock"
-            break;     //The break statement terminates the switch statement, if failed it continues to the next case
+    case 2:
+      computer = 'Paper'
+      break
 
-        case 2:
-            computer = "Paper"
-            break;
+    case 3:
+      computer = 'Scissors'
+      break
 
-        case 3:
-            computer = "Scissors"
-            break;
+    case 4:
+      computer = 'Lizard'
+      break
 
-        case 4:
-            computer = "Lizard"
-            break;
-
-        case 5:
-            computer = "Spock"
-            break;
-
-    }
+    case 5:
+      computer = 'Spock'
+      break
+  }
 }
 
 /** Winner conditions and rules */
 //Dont forget to add 2 more
+
 function checkWinner() {
-    if (user == computer) {
-        return "It's a tie!";
-    }
-    else if (computer == "Rock") {
-        return (user == "Paper") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Rock") {
-        return (user == "Spock") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Paper") {
-        return (user == "Scissor") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Paper") {
-        return (user == "Lizard") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Scissors") {
-        return (user == "Rock") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Scissors") {
-        return (user == "Spock") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Lizard") {
-        return (user == "Rock") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Lizard") {
-        return (user == "Scissors") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Spock") {
-        return (user == "Lizard") ? "Congratulations!" : "Aww you lost!"
-    }
-    else if (computer == "Spock") {
-        return (user == "Paper") ? "Congratulations!" : "Aww you lost!"
-    }
+  if (user === computer) {
+    return "It's a tie!"
+  } else if (computer == 'Rock') {
+    return user == 'Paper' || user == 'Spock'
+      ? 'Congratulations!'
+      : 'Aww you lost!'
+  } else if (computer == 'Paper') {
+    return user == 'Scissors' || user == 'Lizard'
+      ? 'Congratulations!'
+      : 'Aww you lost!'
+  } else if (computer == 'Scissors') {
+    return user == 'Rock' || user == 'Spock'
+      ? 'Congratulations!'
+      : 'Aww you lost!'
+  } else if (computer == 'Lizard') {
+    return user == 'Rock' || user == 'Scissors'
+      ? 'Congratulations!'
+      : 'Aww you lost!'
+  } else if (computer == 'Spock') {
+    return user == 'Lizard' || user == 'Paper'
+      ? 'Congratulations!'
+      : 'Aww you lost!'
+  }
 }
 
+//Points results-
+function setPoints(result) {
+  // Takes "result" variable to check current winning condition
+  if (result === 'Congratulations!') {
+    userPoints++ // Increments user point by one
+  } else if (result === 'Aww you lost!') {
+    computerPoints++ // Increments computer point by one
+  } else if (result === "It's a tie!") {
+    tiePoints++ // Increments when it's a tie
+  }
 
-
-//Points results- 
-function setPoints(result) {  // Takes "result" variable to check current winning condition
-    if (result === "Congratulations!") {
-        userPoints++;    // Increments user point by one
-    } else if (result === "Aww you lost!") {
-        computerPoints++;    // Increments computer point by one
-    } else if (result === "It's a tie!") {
-        tiePoints++;   // Increments when it's a tie
-    }
-
-    //Adds points to the corresponding element 
-    userCounter.textContent = `Your points: ${userPoints}`;
-    computerCounter.textContent = `Computer: ${computerPoints}`;
-    tieCounter.textContent = `Tie!: ${tiePoints}`;
+  //Adds points to the corresponding element
+  userCounter.textContent = `Your points: ${userPoints}`
+  computerCounter.textContent = `Computer: ${computerPoints}`
+  tieCounter.textContent = `Tie!: ${tiePoints}`
 }
-
 
 function finishGame() {
     
